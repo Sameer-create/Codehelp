5d932ac286c500e40fcdd91b667e22a94ea69de7
divyansh5786 <65287346+divyansh5786@users.noreply.github.com>
2021-05-25 14:35:01 +0530

    Update index.js

--- Front-end/src/index.js
+++ Front-end/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+<<<<<<< Updated upstream
 import { BrowserRouter} from "react-router-dom";
 
 
@@ -13,9 +14,28 @@ ReactDOM.render(
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
+=======
+import { BrowserRouter } from "react-router-dom";
+
+
+ReactDOM.render( <
+    React.StrictMode >
+    <
+    BrowserRouter >
+    <
+    App / >
+    <
+    /BrowserRouter> < /
+    React.StrictMode > ,
+    document.getElementById('root')
+>>>>>>> Stashed changes
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+<<<<<<< Updated upstream
 reportWebVitals();
+=======
+reportWebVitals();
+>>>>>>> Stashed changes
